cb982c1c521a85c998665ef0acb08a218ea516a4
TIAGO CAROLINO <6355267@formacao.iefp.pt>
2023-01-26 17:00:54 +0000

    Add project files.

---
+++ DotnetTurma001.ListComponent/Models/ToDo.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetTurma001.ListComponent.Models
+{
+    public class ToDo
+    {
+        public string Description { get; set; }
+        public bool Complete { get; set; }
+    }
+}
